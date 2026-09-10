9b718f358f2d6da63df093554bb8dcac54af4c9e
SkyareCom <skyare.company@gmail.com>
2026-09-10 16:34:49 -0300

    Atualiza cache de misdeal e staff

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="stackup-academy-v17";
-const ASSETS=["./","./index.html","./manifest.webmanifest","./engine.js","./positions-table.js","./fundamentals-details.js","./icon-192.png","./icon-512.png"];
+const CACHE="stackup-academy-v18";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./engine.js","./positions-table.js","./fundamentals-details.js","./misdeal-staff-details.js","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener("activate",event=>{event.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)));await self.clients.claim();const clients=await self.clients.matchAll({type:"window"});await Promise.all(clients.map(client=>client.navigate(client.url).catch(()=>null)));})())});
 self.addEventListener("fetch",event=>{
@@ -11,6 +11,7 @@ self.addEventListener("fetch",event=>{
       let html=await resp.text();
       if(!html.includes("positions-table.js"))html=html.replace("</body>",'<script src="./positions-table.js?v=3"></script></body>');
       if(!html.includes("fundamentals-details.js"))html=html.replace("</body>",'<script src="./fundamentals-details.js?v=3"></script></body>');
+      if(!html.includes("misdeal-staff-details.js"))html=html.replace("</body>",'<script src="./misdeal-staff-details.js?v=1"></script></body>');
       const headers=new Headers(resp.headers);
       headers.set("content-type","text/html; charset=utf-8");
       return new Response(html,{status:resp.status,statusText:resp.statusText,headers});
