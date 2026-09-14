c21e41a5de4b605d418791d658974632b0a11b2d
SkyareCom <skyare.company@gmail.com>
2026-09-14 11:48:44 -0300

    Prepara build Android de producao para Play Store

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -2,6 +2,17 @@ plugins {
     id("com.android.application")
 }
 
+val releaseKeystore = System.getenv("STACKUP_UPLOAD_KEYSTORE")
+val releaseStorePassword = System.getenv("STACKUP_UPLOAD_STORE_PASSWORD")
+val releaseKeyAlias = System.getenv("STACKUP_UPLOAD_KEY_ALIAS")
+val releaseKeyPassword = System.getenv("STACKUP_UPLOAD_KEY_PASSWORD")
+val hasReleaseSigning = listOf(
+    releaseKeystore,
+    releaseStorePassword,
+    releaseKeyAlias,
+    releaseKeyPassword
+).all { !it.isNullOrBlank() }
+
 android {
     namespace = "com.skyare.stackupacademy"
     compileSdk = 36
@@ -14,9 +25,23 @@ android {
         versionName = "1.0.0"
     }
 
+    signingConfigs {
+        if (hasReleaseSigning) {
+            create("release") {
+                storeFile = file(releaseKeystore!!)
+                storePassword = releaseStorePassword
+                keyAlias = releaseKeyAlias
+                keyPassword = releaseKeyPassword
+            }
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
+            if (hasReleaseSigning) {
+                signingConfig = signingConfigs.getByName("release")
+            }
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -31,5 +56,5 @@ android {
 }
 
 dependencies {
-    implementation("com.google.androidbrowserhelper:androidbrowserhelper:2.7.2")
+    implementation("com.google.androidbrowserhelper:androidbrowserhelper:2.7.3")
 }
