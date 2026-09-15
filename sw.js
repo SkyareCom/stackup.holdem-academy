886ec061bfcb65b65bdb30a30adb9d75947ada9a
SkyareCom <skyare.company@gmail.com>
2026-09-15 13:18:27 -0300

    Corrige versão de cache após rollback

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='stackup-academy-rollback-v102';
+const CACHE='stackup-academy-v102';
 const SW_VERSION=102;
 const ASSETS=[
   './','./index.html','./privacy.html','./manifest.webmanifest','./engine.js','./session-reset.js','./language-selector.js','./i18n-en-us-phrases-1.js','./i18n-en-us-phrases-2.js','./i18n-en-us-phrases-3.js','./i18n-en-us-words.js','./i18n-en-us-words-extra-1.js','./i18n-en-us-words-extra-2.js','./i18n-en-us-words-extra-3.js','./i18n-en-us-words-extra-4.js','./i18n-en-us.js',
