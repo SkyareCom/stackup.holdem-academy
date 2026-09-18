fc2ffa5d56fb3ac8af625c302e90ae74fed06d67
SkyareCom <skyare.company@gmail.com>
2026-09-17 23:38:57 -0300

    Bump visual cache after canonical table rollout

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='stackup-academy-v111';
-const SW_VERSION=111;
+const CACHE='stackup-academy-v112';
+const SW_VERSION=112;
 const ASSETS=[
   './fonts/love-ya-like-a-sister.ttf',
   './','./index.html','./privacy.html','./manifest.webmanifest','./engine.js','./session-reset.js','./language-selector.js','./i18n-en-us-phrases-1.js','./i18n-en-us-phrases-2.js','./i18n-en-us-phrases-3.js','./i18n-en-us-words.js','./i18n-en-us-words-extra-1.js','./i18n-en-us-words-extra-2.js','./i18n-en-us-words-extra-3.js','./i18n-en-us-words-extra-4.js','./i18n-en-us.js',
@@ -51,7 +51,7 @@ const SCRIPTS=[
   ['portuguese-corrections.js',5],
   ['cover-layout.js',10],
   ['release-compliance.js',1],
-  ['academy-visual-system.js',4],
+  ['academy-visual-system.js',5],
   ['page-top-reset.js',5],
   ['typography-standard.js',3],
   ['academy-loader.js',15]
