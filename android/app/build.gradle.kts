c7445f1fe664a87998d784969888e944558c21d9
SkyareCom <skyare.company@gmail.com>
2026-09-20 23:53:25 -0300

    Bump Android versionCode to 6

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         applicationId = "com.skyare.stackupacademy"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
+        versionCode = 6
         versionName = "1.0.4"
     }
 
